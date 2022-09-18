dc1becbe33fab9cf1f2557bf4ae44c83e53f98bc
SIDDHARTH SAXENA <siddy5691@gmail.com>
2022-09-18 23:50:51 +0530

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,16 +3,10 @@ import Logo from "../images/photo-grid.png";
 
 export default function Gallery() {
   return (
-    <div>
-    <div>
-      <img src={Logo} alt=""/>
-      </div>
-      <div>
-      <h2>All made possible by hosts on Airbnb.</h2>
-      <p>Search & Book Your Stay on Airbnb.co.in Today. Powered By Locals. Enjoyed By All. Entire Homes. Full Kitchens. Swimming Pools. Air Conditioning. Wifi.
-
-Types: Apartments, Rooms, Flats, Entire Homes, B&Bs</p>
-    </div>
+    <div className=''>
+      <img src={Logo} className='' alt=""/>
+      <h2 className=''>All made possible by hosts on Airbnb.</h2>
+      <p className=''>Search & Book Your Stay on Airbnb.co.in Today. Powered By Locals. Enjoyed By All. Entire Homes. Full Kitchens. Swimming Pools. Air Conditioning. Wifi.Types: Apartments, Rooms, Flats, Entire Homes, B&Bs</p>
     </div>
   )
 }
